36242b48b50c1bff7a5f71a9f3e0f56425eb92f4
hanna <hlbernard124@gmail.com>
2017-03-28 14:11:26 -0400

    level 1

---
+++ HumansPandasRobots/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumansPandasRobots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Human elliott = new Human()
+            {
+                Name = "Elliott",
+                Food = "Lasagna",
+            };
+
+            Console.WriteLine(elliott.Name);
+            Console.WriteLine(elliott.DisplayGreeting());
+            Console.WriteLine(elliott.Eat());
+            Console.WriteLine(elliott.GoToSleep());
+            Console.WriteLine(elliott.WakeUp());
+
+            Panda ceail = new Panda()
+            {
+                Name = "Ceail",
+                Food = "Bamboo",
+            };
+
+            Console.WriteLine(ceail.Name);
+            Console.WriteLine(ceail.DisplayGreeting());
+            Console.WriteLine(ceail.Eat());
+            Console.WriteLine(ceail.GoToSleep());
+            Console.WriteLine(ceail.WakeUp());
+
+            Robot titan = new Robot()
+            {
+                Name = "Titan",
+                TerminatorHere = false,
+            };
+
+            Console.WriteLine(titan.Name);
+            Console.WriteLine(titan.DisplayGreeting());
+            Console.WriteLine(titan.StartUp());
+            Console.WriteLine(titan.ShutDown());
+            Console.WriteLine(titan.IsATerminator());
+
+
+        }
+    }
+}
---
+++ HumansPandasRobots/Robot.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumansPandasRobots
+{
+    class Robot
+    {
+        public string Name { get; set; }
+        public bool IsASleep { get; set; }
+        public bool TerminatorHere { get; set; } = true;
+
+        public string DisplayName()
+        {
+            return this.Name = Name;
+        }
+        public string DisplayGreeting()
+        {
+            return $"Hello I am a Robot named {this.Name}";
+        }
+
+        public string StartUp()
+        {
+            return "Started...";
+        }
+        public string ShutDown()
+        {
+            if (this.IsASleep)
+            {
+                return true.ToString();
+            }
+            return "Shutdown...";
+        }
+
+        public bool IsATerminator()
+        {
+            if (TerminatorHere)
+            {
+                Console.WriteLine("I am the terminator");
+            }else
+            {
+                Console.WriteLine("I am not a terminator");
+            }
+
+            return TerminatorHere;
+        }
+    }
+}
